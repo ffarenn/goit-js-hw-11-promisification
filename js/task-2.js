5310db82937ea75de0afbd6b1378f2378f0d89b3
Andrejs Klimza <andrejsklimza@gmail.com>
2021-10-13 00:10:55 +0100

    adds homework task code

---
+++ js/task-2.js
@@ -0,0 +1,44 @@
+// // Задание 2 - 1й вариант 
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+  return Promise.resolve(updatedUsers); // Использовал этот метод
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
+
+
+// // Задание 2 - второй вариант, если первый не подходит
+
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = (allUsers, userName) => {
+//   return new Promise((resolve) => {
+//     resolve(allUsers.map(user =>
+//       user.name === userName ? { ...user, active: !user.active } : user,
+//     ));
+//   })
+// };
+
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
